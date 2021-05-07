96dc0aac5993ea0e6cda2450c61bd0d3c6f9f829
brookesargent <brookesargent01@gmail.com>
2021-05-07 13:54:59 -0400

    update region to us-east-1

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 variable "region" {
-  default     = "us-east-2"
+  default     = "us-east-1"
   description = "AWS region"
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {}
@@ -22,7 +22,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.66.0"
 
-  name                 = "education-vpc"
+  name                 = "waypoint-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
